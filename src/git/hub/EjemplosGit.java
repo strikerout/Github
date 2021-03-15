5561c2e983e3ed3ed9d77da04936afee17c7853c
strikerout <dani3l.uebp@gmail.com>
2021-03-14 22:11:14 -0300

    Commit de rama B

--- src/git/hub/EjemplosGit.java
+++ src/git/hub/EjemplosGit.java
@@ -3,6 +3,6 @@ package git.hub;
 public class EjemplosGit {
     public static void main(String[] args) {
         System.out.println("Hola");
-        System.out.println("Este es un ejemplo de rama-A");
+        System.out.println("Este es un ejemplo de Rama-B");
     }
 }
