c3c75f26a788a881215151c9b10bec925f947954
strikerout <dani3l.uebp@gmail.com>
2021-03-17 21:54:14 -0300

    Rama A Conflict

--- src/git/hub/EjemplosGit.java
+++ src/git/hub/EjemplosGit.java
@@ -3,6 +3,7 @@ package git.hub;
 public class EjemplosGit {
     public static void main(String[] args) {
         System.out.println("Hola");
+
         System.out.println("Este es un ejemplo de rama-A");
         for (int i=0; i<10; i++){
             System.out.println("La puta madre esto no me hace conflicto xdddd");
