4f7813616212e50fa30a6bc67513b9c1e1a5d06f
strikerout <dani3l.uebp@gmail.com>
2021-03-18 21:09:42 -0300

    Prueba de conflicto

--- src/git/hub/EjemplosGit.java
+++ src/git/hub/EjemplosGit.java
@@ -5,10 +5,7 @@ public class EjemplosGit {
         System.out.println("Hola");
 
 
-        System.out.println("Este es un ejemplo de Rama-B");
-        for (int i = 0; i < 5; i++) {
-            System.out.println("La puta madre esto no me hace conflicto  BBBBB xdddd");
-        }
+
     }
 }
 
